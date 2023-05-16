19cd8073167a0a404f3648d86e0b3a63026e3e1c
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2023-05-16 12:17:23 -0600

    Went over app lifecycle, created a Appetizer model and mock data, overview of decodable and json

---
+++ AppetizersAppSA/Appetizer.swift
@@ -0,0 +1,39 @@
+//
+//  Appetizer.swift
+//  AppetizersAppSA
+//
+//  Created by Mitch Andrade on 5/16/23.
+//
+
+import Foundation
+
+struct Appetizer: Decodable {
+    let id: Int
+    let name: String
+    let description: String
+    let price: Double
+    let imageURL: String
+    let calories: Int
+    let protein: Int
+    let carbs: Int
+}
+
+struct AppetizerResponse {
+    let request: [Appetizer]
+}
+
+struct MockData {
+    
+    static let sampleAppetizer = Appetizer(
+        id: 0001,
+        name: "Test Appetizer",
+        description: "This is the description for my appetizer",
+        price: 9.99,
+        imageURL: "",
+        calories: 467,
+        protein: 70,
+        carbs: 87
+    )
+    
+    static let appetizers = [sampleAppetizer, sampleAppetizer, sampleAppetizer, sampleAppetizer]
+}
