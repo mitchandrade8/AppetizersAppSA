5a47202900fc8fd12e9c2af036ebd6f419d17d10
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2023-05-16 10:21:01 -0600

    Initial Commit

---
+++ AppetizersAppSA/AppetizersAppSAApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppetizersAppSAApp.swift
+//  AppetizersAppSA
+//
+//  Created by Mitch Andrade on 5/16/23.
+//
+
+import SwiftUI
+
+@main
+struct AppetizersAppSAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
